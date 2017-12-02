959418436562cba9129384af3ea935c39e21eaf4
Richard Bernard <bernard.rich97@gmail.com>
2017-12-02 17:07:27 -0500

    Initial Commit

---
+++ Fundamentals/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fundamentals
+//
+//  Created by Richard Bernard on 2017-12-02.
+//  Copyright © 2017 Richard Bernard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
