6c186eea7978fa696d08848d55915e8c1a42f09b
Richard Bernard <bernard.rich97@gmail.com>
2017-12-03 12:48:20 -0500

    My first commit?

--- Fundamentals/ViewController.swift
+++ Fundamentals/ViewController.swift
@@ -10,9 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var maybeLabel: UILabel!
+    
+    var pressCount = 0
+    var otherPressCount = 0
+    
+    @IBAction func beginPressed(_ sender: Any) {
+        pressCount = pressCount + 1
+        if(pressCount>=10){
+            maybeLabel.text = "Quit pressing that"
+        }
+    }
+    
+    @IBAction func otherPushed(_ sender: Any) {
+        otherPressCount = otherPressCount + 1
+        if(otherPressCount>=10){
+            maybeLabel.text = "That too"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.darkGray
+        maybeLabel.text = "This is the title!"
     }
 
     override func didReceiveMemoryWarning() {
